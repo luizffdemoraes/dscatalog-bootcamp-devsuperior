dd60bbffb5bc71056a18fc29a825475c31751ba1
Luiz Moraes <lffm1994@gmail.com>
2021-05-18 20:59:08 -0300

    Database seeding

--- backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.devsuperior.dscatalog.entities.Category;
 import com.devsuperior.dscatalog.repositories.CategoryRepository;
@@ -14,6 +15,12 @@ public class CategoryService {
 	@Autowired
 	private CategoryRepository repository;
 
+	/*
+	 * Garante que o metodo vai executar uma transação com banco de dados e não vai
+	 * travar evita um lock no banco de dados
+	 */
+
+	@Transactional(readOnly = true)
 	public List<Category> findAll() {
 		return repository.findAll();
 

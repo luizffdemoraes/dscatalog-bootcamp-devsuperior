370b3bc7b398c5aad96e9744f53583a1af9223d7
Luiz Moraes <lffm1994@gmail.com>
2021-05-20 00:18:40 -0300

    DTO

--- backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
@@ -1,11 +1,15 @@
 package com.devsuperior.dscatalog.services;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.devsuperior.dscatalog.dto.CategoryDTO;
 import com.devsuperior.dscatalog.entities.Category;
 import com.devsuperior.dscatalog.repositories.CategoryRepository;
 
@@ -21,9 +25,27 @@ public class CategoryService {
 	 */
 
 	@Transactional(readOnly = true)
-	public List<Category> findAll() {
-		return repository.findAll();
-
+	public List<CategoryDTO> findAll() {
+		List<Category> list = repository.findAll();
+		
+		/* Implementação com expressão Lambda
+		 * stream converter sua coleção, stream um recurso para trabalhar com
+		 * funções de alta ordem inclusive lambda para aplicar transformações
+		 * map aplica um função transforma cada elemento da lista em outra coisa
+		 * ela aplica uma função a cada elemento.
+		 * collec transforma uma stream em uma lista
+		 */
+		
+		return list.stream()
+				.map(x -> new CategoryDTO(x))
+				.collect(Collectors.toList());
+		
+		//Uma forma de converter a lista de category
+//		List<CategoryDTO> listDto = new ArrayList<>();
+//		for(Category cat : list) {
+//			listDto.add(new CategoryDTO(cat));
+//		}
+//		return listDto;
 	}
 
 }

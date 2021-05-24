e7b27f4dd0134cbe0b98fd5b33b230a5041a6638
Luiz Moraes <lffm1994@gmail.com>
2021-05-24 01:24:38 -0300

    Product repository, service, resource, GET ok

---
+++ backend/src/main/java/com/devsuperior/dscatalog/resources/ProductResource.java
@@ -0,0 +1,94 @@
+package com.devsuperior.dscatalog.resources;
+
+import java.net.URI;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import com.devsuperior.dscatalog.dto.ProductDTO;
+import com.devsuperior.dscatalog.services.ProductService;
+
+/*
+ * Pré processamento ao compilar fornecendo a estrutura para funcionar corretamente.
+ * Definição de rota.
+ * No java alguns tipos são interfaces exemplo List. 
+ * Não pode ser instanciada uma interface, no caso 
+ * devemos instanciar uma Classe que implemente a interface exemplo ArrayList.
+ * ResponseEntity.ok() Resposta 200
+ * .body defini o corpo da resposta
+ * 
+ */
+
+@RestController
+@RequestMapping(value = "/products")
+public class ProductResource {
+
+	@Autowired
+	private ProductService service;
+
+	/*
+	 * Consulta geral Alteração de retorno List para Page
+	 * 
+	 * @RequestParam significa que é um parametro opcional - pagina, quantidade de
+	 * registros, nome de ordenação e direção ASC OU DESC
+	 */
+
+	@GetMapping
+	public ResponseEntity<Page<ProductDTO>> findAll(@RequestParam(value = "page", defaultValue = "0") Integer page,
+			@RequestParam(value = "linesPerPage", defaultValue = "12") Integer linesPerPage,
+			@RequestParam(value = "direction", defaultValue = "ASC") String direction,
+			@RequestParam(value = "orderBy", defaultValue = "name") String orderBy
+
+	) {
+
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+
+		Page<ProductDTO> list = service.findAllPaged(pageRequest);
+
+		return ResponseEntity.ok().body(list);
+
+	}
+
+	// consulta por ID, anotação @PathVariable reconhecer o parametro de argumento
+	// da rota.
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<ProductDTO> findById(@PathVariable Long id) {
+		ProductDTO dto = service.findById(id);
+		return ResponseEntity.ok().body(dto);
+
+	}
+
+	@PostMapping
+	public ResponseEntity<ProductDTO> insert(@RequestBody ProductDTO dto) {
+		dto = service.insert(dto);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(dto.getId()).toUri();
+		return ResponseEntity.created(uri).body(dto);
+
+	}
+
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<ProductDTO> update(@PathVariable Long id, @RequestBody ProductDTO dto) {
+		dto = service.update(id, dto);
+		return ResponseEntity.ok().body(dto);
+	}
+
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Long id) {
+		service.delete(id);
+		return ResponseEntity.noContent().build();
+	}
+
+}
---
+++ backend/src/main/java/com/devsuperior/dscatalog/services/ProductService.java
@@ -0,0 +1,112 @@
+package com.devsuperior.dscatalog.services;
+
+import java.util.Optional;
+
+import javax.persistence.EntityNotFoundException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.devsuperior.dscatalog.dto.ProductDTO;
+import com.devsuperior.dscatalog.entities.Product;
+import com.devsuperior.dscatalog.repositories.ProductRepository;
+import com.devsuperior.dscatalog.services.exceptions.DataBaseException;
+import com.devsuperior.dscatalog.services.exceptions.ResourceNotFoundException;
+
+@Service
+public class ProductService {
+
+	@Autowired
+	private ProductRepository repository;
+
+	/*
+	 * Método para retornar todos registros do banco de dados. Garante que o metodo
+	 * vai executar uma transação com banco de dados e não vai travar evita um lock
+	 * no banco de dados Implementação com expressão Lambda stream converter sua
+	 * coleção, stream um recurso para trabalhar com funções de alta ordem inclusive
+	 * lambda para aplicar transformações map aplica um função transforma cada
+	 * elemento da lista em outra coisa ela aplica uma função a cada elemento.
+	 * collec transforma uma stream em uma lista alteração do metodo de listagem
+	 * para paginação o page já é um stream
+	 */
+
+	@Transactional(readOnly = true)
+	public Page<ProductDTO> findAllPaged(PageRequest pageRequest) {
+		Page<Product> list = repository.findAll(pageRequest);
+		return list.map(x -> new ProductDTO(x));
+	}
+
+// Conversão de lista de Categoria para uma Lista de Categoria Dto com função Lambda
+// return list.stream().map(x -> new ProductDTO(x)).collect(Collectors.toList());
+//	
+//		Uma forma de converter a lista de category
+//		List<ProductDTO> listDto = new ArrayList<>();
+//		for(Product cat : list) {
+//			listDto.add(new ProductDTO(cat));
+//		}
+//		return listDto;
+
+	/*
+	 * criar metodo para busca de id -> Optional foi criado para não trabalhar com
+	 * valor nulo a partir do Java 8 -> .get do Optional obtem o objeto dentro do
+	 * Optional -> .orElse Tentar obter o objeto que seria o get, se o objeto não
+	 * estiver presente no Optional você pode retornar outra coisa um objeto
+	 * alternativo, nulo exception -> orElseThrow() permite definir uma chamada de
+	 * exception uma função lambda
+	 */
+
+	@Transactional(readOnly = true)
+	public ProductDTO findById(Long id) {
+		Optional<Product> obj = repository.findById(id);
+		Product entity = obj.orElseThrow(() -> new ResourceNotFoundException("Entity not found"));
+		return new ProductDTO(entity, entity.getCategories());
+	}
+
+	@Transactional
+	public ProductDTO insert(ProductDTO dto) {
+		Product entity = new Product();
+		//entity.setName(dto.getName());
+		entity = repository.save(entity);
+		return new ProductDTO(entity);
+	}
+
+	/*
+	 * Diferenda de findById e getOne. findById: Efetiva o acesso ao banco de dados
+	 * ele traz do banco de dados. getOne: Ele não toca no banco de dados ele
+	 * instancia um objeto provisorio e com o id do objeto so quando mandar salvar
+	 * ele acessa o banco.
+	 */
+
+	@Transactional
+	public ProductDTO update(Long id, ProductDTO dto) {
+		try {
+			Product entity = repository.getOne(id);
+			//entity.setName(dto.getName());
+			entity = repository.save(entity);
+			return new ProductDTO(entity);
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException("Id not found " + id);
+		}
+	}
+
+	// captura de exception para isso não colocamos @Transactional
+	// captura exception de integridade quando deletamos algo que não podemos
+	public void delete(Long id) {
+		try {
+			repository.deleteById(id);
+
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException("Id not found " + id);
+		} catch (DataIntegrityViolationException e) {
+
+			throw new DataBaseException("Integrity violation");
+		}
+
+	}
+
+}

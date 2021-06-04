e1b3fdc8a0d31f22d67423c57ba09b756c150ebb
Luiz Moraes <lffm1994@gmail.com>
2021-06-04 03:45:32 -0300

    method Delete ok

--- backend/src/main/java/com/devsuperior/dscatalog/services/ProductService.java
+++ backend/src/main/java/com/devsuperior/dscatalog/services/ProductService.java
@@ -111,6 +111,7 @@ public class ProductService {
 
 		} catch (EmptyResultDataAccessException e) {
 			throw new ResourceNotFoundException("Id not found " + id);
+			
 		} catch (DataIntegrityViolationException e) {
 
 			throw new DataBaseException("Integrity violation");

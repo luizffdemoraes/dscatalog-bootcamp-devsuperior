1bf2dd6e171bf5f279a68377cf9e83dcd5bfe3a8
Luiz Moraes <lffm1994@gmail.com>
2021-07-22 23:22:12 -0300

    Imports estáticos do Mockito

--- backend/src/test/java/com/devsuperior/dscatalog/services/ProductServiceTest.java
+++ backend/src/test/java/com/devsuperior/dscatalog/services/ProductServiceTest.java
@@ -1,6 +1,9 @@
 package com.devsuperior.dscatalog.services;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
@@ -8,7 +11,6 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
@@ -36,8 +38,8 @@ public class ProductServiceTest {
 		nonExistingId = 1000L;
 		
 		// Ao criar um mock e necessário configurar o comportamento simulado dele.
-		Mockito.doNothing().when(repository).deleteById(existingId);
-		Mockito.doThrow(EmptyResultDataAccessException.class).when(repository).deleteById(nonExistingId);
+		doNothing().when(repository).deleteById(existingId);
+		doThrow(EmptyResultDataAccessException.class).when(repository).deleteById(nonExistingId);
 	}
 	
 	@Test
@@ -51,7 +53,7 @@ public class ProductServiceTest {
 		});
 		
 		//Verificação
-		Mockito.verify(repository, Mockito.times(1)).deleteById(existingId);
+		verify(repository, times(1)).deleteById(existingId);
 		
 	}
 	

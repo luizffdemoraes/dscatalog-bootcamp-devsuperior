010639ccd137db9f03e3bab6d8e4cbc12496ffee
Luiz Moraes <lffm1994@gmail.com>
2021-07-25 17:44:17 -0300

    02-27 Simulando comportamentos diversos com Mockito

--- backend/src/test/java/com/devsuperior/dscatalog/services/ProductServiceTest.java
+++ backend/src/test/java/com/devsuperior/dscatalog/services/ProductServiceTest.java
@@ -5,19 +5,29 @@ import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentMatcher;
+import org.mockito.ArgumentMatchers;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import com.devsuperior.dscatalog.entities.Product;
 import com.devsuperior.dscatalog.repositories.ProductRepository;
 import com.devsuperior.dscatalog.services.exceptions.DataBaseException;
 import com.devsuperior.dscatalog.services.exceptions.ResourceNotFoundException;
+import com.devsuperior.dscatalog.tests.Factory;
 
 @ExtendWith(SpringExtension.class)
 public class ProductServiceTest {
@@ -35,19 +45,30 @@ public class ProductServiceTest {
 	private long existingId;
 	private long nonExistingId;
 	private long dependentId;
+	private PageImpl<Product> page; //Tipo concreto que representa uma pagina de dados
+	private Product product;
 	
 	@BeforeEach
 	void setUp() throws Exception {
 		existingId = 1L;
-		nonExistingId = 1000L;
-		dependentId = 4L;
-		
+		nonExistingId = 2L;
+		dependentId = 3L;
+		product = Factory.createProduct();
+		page = new PageImpl<>(List.of(product));
 		
 		// Ao criar um mock e necessário configurar o comportamento simulado dele.
+		//Simular o comportamento de uma pagina
+		Mockito.when(repository.findAll((Pageable)ArgumentMatchers.any())).thenReturn(page);
+		
+		//Simular o comportamento de save
+		Mockito.when(repository.save(ArgumentMatchers.any())).thenReturn(product);
+		
+		//Simular a busca por id existente e inexistente
+		Mockito.when(repository.findById(existingId)).thenReturn(Optional.of(product)); // .of recebe um objeto
+		Mockito.when(repository.findById(nonExistingId)).thenReturn(Optional.empty()); // empty instancia um Optional vazio
+		
 		doNothing().when(repository).deleteById(existingId);
-	
 		doThrow(EmptyResultDataAccessException.class).when(repository).deleteById(nonExistingId);
-	
 		doThrow(DataIntegrityViolationException.class).when(repository).deleteById(dependentId);
 		
 	}

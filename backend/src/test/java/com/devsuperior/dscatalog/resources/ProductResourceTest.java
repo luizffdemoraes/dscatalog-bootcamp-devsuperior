7ab40b97911d10ab7a24c30ad71dd25f75fff06d
luizffdemoraes <lffm1994@gmail.com>
2021-07-28 23:32:11 -0300

    02-30 Começando testes na camada web

---
+++ backend/src/test/java/com/devsuperior/dscatalog/resources/ProductResourceTest.java
@@ -0,0 +1,54 @@
+package com.devsuperior.dscatalog.resources;
+
+import com.devsuperior.dscatalog.dto.ProductDTO;
+import com.devsuperior.dscatalog.services.ProductService;
+import com.devsuperior.dscatalog.tests.Factory;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(ProductResource.class)
+public class ProductResourceTest {
+
+    //Para realizar a chamada de endpoints uma das abordagens e utilizar o MockMvc
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private ProductService service;
+
+    private ProductDTO productDTO;
+    private PageImpl<ProductDTO> page; //Instanciando objeto concreto
+
+    /*
+        Setar variaveis e simular comportamento.
+     */
+    @BeforeEach
+    void setUp() throws Exception {
+
+        productDTO = Factory.createProductDTO();
+        page = new PageImpl<>(List.of(productDTO));
+
+        when(service.findAllPaged(any())).thenReturn(page);
+    }
+
+    @Test
+    public void findAllShouldReturnPage() throws Exception {
+        mockMvc.perform(get("/products")).andExpect(status().isOk());
+    }
+
+}
